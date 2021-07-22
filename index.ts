ee46859fe2b7a2785b8c655f4593ef019ac22f44
ubkychin <anguyen@swin.edu.au>
2021-07-22 11:43:44 +1000

    1 file modified

--- index.ts
+++ index.ts
@@ -33,25 +33,80 @@ function helloWorld() {
 const runButton: HTMLElement = document.getElementById('run-btn');
 runButton.addEventListener('click', fizzbuzz);
 
+const output: HTMLElement = document.getElementById('output');
+
 function fizzbuzz() {
   let fizzBuzzCounter: number = 1;
 
   while (fizzBuzzCounter <= 100) {
     if (fizzBuzzCounter % 5 == 0 && fizzBuzzCounter % 3 == 0) {
-      console.log("FizzBuzz, " + fizzBuzzCounter);
+      output.innerHTML += `<p>Fizzbuzz ${fizzBuzzCounter}</p>`;
     } else if(fizzBuzzCounter % 3 == 0) {
-      console.log("Fizz, " + fizzBuzzCounter);
+      output.innerHTML += `<p>Fizz ${fizzBuzzCounter}</p>`;
     } else if (fizzBuzzCounter % 5 == 0) {
-      console.log("Buzz, " + fizzBuzzCounter);
+      output.innerHTML += `<p>Buzz ${fizzBuzzCounter}</p>`;
     }  else {
-      console.log(fizzBuzzCounter);
+      output.innerHTML += `<p>${fizzBuzzCounter}</p>`;
     }
 
     fizzBuzzCounter++;
   }
 
-
 }
+
+//--------------------- CLASSES -----------------------------------------
+class Car {
+  rego: string;
+  speed: number;
+
+  constructor (rego:string) {
+    this.rego = rego;
+    this.speed = 0;
+  }
+
+  //TODO: don't allow speed to go over 120
+  accelerate(): void {
+    if(this.speed <= 120) {
+      this.speed += 10;
+    }
+
+    if(this.speed > 120) {
+      this.speed = 120;
+    }
+    
+  }
+}
+//------------------------ OBJECTS ------------------------------
+const accelerateButton:HTMLElement = document.getElementById('accelerate-btn');
+
+
+let c1: Car = new Car("ABC123");
+console.log(c1.speed);
+
+accelerateButton.addEventListener('click', accelerateCar);
+
+function accelerateCar() {
+  c1.accelerate();
+  displayCarInfo(c1);
+}
+
+function displayCarInfo(car: Car) {
+  const display: HTMLElement = document.getElementById('carDisp');
+  display.innerHTML = `Rego: ${car.rego} | Speed: ${car.speed}`;
+}
+
+//---------------- ARRAYS --------------------------
+
+let cars: Array<Car> = [];
+cars.push(new Car("ABC123"));
+cars.push(new Car("ZXC321"));
+cars.push(new Car("QWF789"));
+
+cars[1].speed = 10;
+cars[2].speed = 20;
+
+console.log(cars[1].speed);
+//-----------------------------------------------
 
 // var is function scoped and let is block scoped
 
